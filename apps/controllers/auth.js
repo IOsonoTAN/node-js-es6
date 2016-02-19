ceb58e51c7a9823248e97baafb7f1bbc75966c49
Krissada Boontrigratn <krissada@paysbuy.co.th>
2016-02-19 10:50:28 +0700

    clean code

--- apps/controllers/auth.js
+++ apps/controllers/auth.js
@@ -2,16 +2,10 @@
 
 require('rootpath')()
 
-const config = require('config/app'),
-      passport = require('passport'),
-      googleStrategy = require('passport-google').Strategy
+const config = require('config/app')
 
 exports.getProfile = (req, res) => {
   console.log('Domain ->', req.session.passport.user._json.domain)
   console.log('Profile ->', req.session.passport.user)
   res.send('getProfile')
-}
-
-exports.getAuthGoogleCallback = (req, res) => {
-  res.send('Logged')
 }
